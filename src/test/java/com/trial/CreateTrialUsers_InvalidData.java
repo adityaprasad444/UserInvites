5f9d1791a439fb4ed37c4f47aee2ecc9e247ac70
Aditya Prasad <y.adityaprasad@gmail.com>
2021-04-26 19:15:33 +0530

    Added few scenarios

--- src/test/java/com/trial/CreateTrialUsers_InvalidData.java
+++ src/test/java/com/trial/CreateTrialUsers_InvalidData.java
@@ -148,6 +148,8 @@ public class CreateTrialUsers_InvalidData {
 		int statusCode=res.getStatusCode();
 		Assert.assertEquals(statusCode, 204, "Trial Access Not Success");
 		data.startConnection("test");
+		String DbData1=data.getdata("id", "email", "status", "userinvitelogs", "email", traildata.get("email").toLowerCase());
+		Assert.assertEquals(DbData1.contains("failed"), true, "Trial user creation failed");
 		String DbData=data.getdata("id", "email", "userType", "users", "email", traildata.get("email").toLowerCase());
 		Assert.assertEquals(DbData.contains("trial"), true, "Trial User not crated");
 		String user=DbData.substring(14,38);
@@ -155,4 +157,41 @@ public class CreateTrialUsers_InvalidData {
 		data.deleteWithString("users", "email", traildata.get("email").toLowerCase());
 		data.closeConnection();
 	}
+	
+	@SuppressWarnings("unchecked")
+	@Test(priority =6)
+	public void Email_blank(){
+		RestAssured.baseURI=ch.testURL();
+		HashMap<String,String> traildata=ExcelRead.readTestData("Trialusers","Email_blank");
+		RequestSpecification request=RestAssured.given();
+		JSONObject authparam = new JSONObject();
+		JSONArray authArray = new JSONArray();
+		JSONObject requestParams = new JSONObject();
+		requestParams.put("userId", Integer.parseInt(traildata.get("id")));
+		requestParams.put("firstName",traildata.get("Fname")); 
+		requestParams.put("lastName", traildata.get("Lname"));
+		requestParams.put("email", traildata.get("email"));
+			authparam.put("wsmCourse", Integer.parseInt(traildata.get("wsmCourse")));
+			authparam.put("accessDays", Integer.parseInt(traildata.get("accessDays")));
+			authparam.put("deliveryType", Integer.parseInt(traildata.get("deliveryType")));
+			authparam.put("websiteUrl",traildata.get("websiteUrl"));
+		authArray.add(authparam);
+		requestParams.put("course",authparam);
+		request.header("Content-Type", "application/json");
+		request.body(requestParams.toJSONString().toString());
+		System.out.println(requestParams.toJSONString());
+		Response res=request.request(Method.POST, "/trialAccess");
+		int statusCode=res.getStatusCode();
+		Assert.assertEquals(statusCode, 204, "Trial Access Not Success");
+		data.startConnection("test");
+		String DbData1=data.getdata("id", "email", "status", "userinvitelogs", "email", traildata.get("email").toLowerCase());
+		Assert.assertEquals(DbData1.contains("failed"), true, "Trial user creation failed");
+		String DbData=data.getdata("id", "email", "userType", "users", "email", traildata.get("email").toLowerCase());
+		Assert.assertEquals(DbData.contains("trial"), true, "Trial User not crated");
+		String user=DbData.substring(14,38);
+		data.deleteByObjectID("usercourses", "user", user);
+		data.deleteWithString("users", "email", traildata.get("email").toLowerCase());
+		data.closeConnection();
+	}
+	
 }
